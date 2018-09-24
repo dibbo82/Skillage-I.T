15fc088cbe61573fdd680fba275c056011ec49c5
drew dibden <drewd.work@gmai.com>
2018-09-24 19:08:24 +1000

    Running test on .ods file changing text in row 3

--- test csv.py
+++ test csv.py
@@ -4,10 +4,11 @@ with open('Wood Stock.csv','r') as csv_file:
     csv_reader = csv.reader(csv_file)
     
     with open('New Wood Stock.csv','w') as new_file:
-        csv_writer = csv.writer(new_file, delimiter='_')
+        csv_writer = csv.writer(new_file)
         
         for line in csv_reader:
             csv_writer.writerow(line)
+            
             
             
             
