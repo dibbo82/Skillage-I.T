7e7c37b3189dd66a178ee61499e5a85ddd3f264b
drew dibden <drewd.work@gmai.com>
2018-09-24 20:27:26 +1000

    Running test on .ods files to html for web interface

--- test csv.py
+++ test csv.py
@@ -1,12 +1,13 @@
 import csv
+import html
 
 with open('Wood Stock.csv','r') as csv_file:
     csv_reader = csv.reader(csv_file)
     
     with open('New Wood Stock.csv','w') as new_file:
         csv_writer = csv.writer(new_file)
-        
-        for line in csv_reader:
+       
+            for line in csv_reader:
             csv_writer.writerow(line)
             
             
