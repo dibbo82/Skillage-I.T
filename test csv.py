89118e4b2fd024988552813b6d52d6c05060f1d3
drew dibden <drewd.work@gmai.com>
2018-09-24 16:26:13 +1000

    Added new csv. python code for changing a csv. file

---
+++ test csv.py
@@ -0,0 +1,12 @@
+import csv
+
+with open('Wood Stock.csv','r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    
+    with open('New Wood Stock.csv','w') as new_file:
+        csv_writer = csv.writer(new_file, delimiter='_')
+        
+        for line in csv_reader:
+            csv_writer.writerow(line)
+                
+            
